818479f7abdbfdae7cf841709d4615082bcc951b
iamaniketgupta <aniketgupta.org@gmail.com>
2024-02-17 03:18:19 +0530

    fix : some code

--- trackexpense/src/components/ExpenseTable.jsx
+++ trackexpense/src/components/ExpenseTable.jsx
@@ -1,43 +1,57 @@
 import { useEffect, useState } from "react";
+import ContextMenu from "./ContextMenu";
 
-const ExpenseTable = (props) => {
-    let expenseData = props.expense;
+const ExpenseTable = ({ expense, setExpense, totalAmount, setTotalAmount }) => {
 
-    const uniqueCategories = Array.from(new Set(expenseData.map(expense => expense.category)));
-    const [filterdata, setFilterData] = useState(expenseData);
+    const [filterdata, setFilterData] = useState([])
+    const[contextPosition,setContextPosition] =useState({});
+
+
+    useEffect(() => {
+        setFilterData(expense);
+    }, [expense]);
+
+    const uniqueCategories = Array.from(new Set(expense.map(elem => elem.category)));
 
     const filterByCategory = (e) => {
-        const selectedOption = e.target.value
-
-        const filteredCategory = selectedOption ? expenseData.filter((elem) => elem.category === selectedOption) : expenseData;
+        const selectedOption = e.target.value;
+        const filteredCategory = selectedOption ? expense.filter((elem) => elem.category === selectedOption) : expense;
         setFilterData(filteredCategory);
     }
 
     useEffect(() => {
         let total = 0;
         filterdata.forEach((element) => {
-            total += parseInt(element.amount);
+            total += parseFloat(element.amount);
         });
-        props.setTotalAmount(total);
+        setTotalAmount(total);
     }, [filterdata]);
 
+    // context handler
+    function contextHandler(e,ID) {
+        e.preventDefault();        
+        
+        const position = {X :e.clientX ,Y:e.clientY,ID:ID}
+        setContextPosition(position);
+    }
 
 
     return (
         <div className="flex overflow-x-hidden">
-            <table className="w-full p-3">
+        <ContextMenu contextPosition={contextPosition} setContextPosition={setContextPosition} expense={expense} setExpense={setExpense}/>
+            <table className="w-full p-3" onClick={()=>setContextPosition({})}>
                 <thead className="bg-gray-50">
                     <tr>
-                        <th className=" py-3 px-6 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th className="py-3 px-6 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Title
                         </th>
-                        <th className=" py-3 px-6 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th className="py-3 px-6 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                             <select name="filterCategory" onChange={filterByCategory} >
                                 <option hidden>Category</option>
-                                <option value={''} default>All</option>
+                                <option value={''}>All</option>
                                 {
-                                    uniqueCategories.map((elem,idx) =>
-                                    <option key={idx} value={elem} title={elem}>{elem}</option>
+                                    uniqueCategories.map((elem, idx) =>
+                                        <option key={idx} value={elem} title={elem}>{elem}</option>
                                     )
                                 }
                             </select>
@@ -49,22 +63,21 @@ const ExpenseTable = (props) => {
                 </thead>
                 <tbody>
                     {filterdata.map((element) => (
-                        <tr key={element.id} className="bg-white">
+                        <tr key={element.id} className="bg-white cursor-pointer" onContextMenu={(e) => contextHandler(e,element.id)}>
                             <td className="text-center py-4 text-sm text-gray-900">{element.title}</td>
-                            <td className="text-center py-4  text-sm text-gray-900">{element.category}</td>
-                            <td className="text-center py-4  text-sm text-gray-900">{element.amount}</td>
+                            <td className="text-center py-4 text-sm text-gray-900">{element.category}</td>
+                            <td className="text-center py-4 text-sm text-gray-900">{element.amount}</td>
                         </tr>
                     ))}
                     <tr className="bg-gray-50">
                         <td colSpan="2" className="text-left px-6 py-4 text-sm font-medium text-gray-900">
                             Total
                         </td>
-                        <td className=" py-4  text-sm text-gray-900">{props.totalAmount}</td>
+                        <td className="py-4 text-sm text-gray-900">{totalAmount}</td>
                     </tr>
                 </tbody>
             </table>
         </div>
-
 
     );
 }
